9b271a042fd22808b104c1e206bc36ae3e0eb24a
Thuyatun <thuratundev@gmail.com>
2022-07-14 15:02:24 +0630

    To do list update

--- animation.js
+++ animation.js
@@ -1,20 +1,20 @@
-
-function addList(){
+function addList() {
     var input = document.getElementById('input');
     var list = document.createElement('li');
-    list.setAttribute('class','list-group-item');
+    list.setAttribute("class", "list-group-item");
     list.append(input.value);
 
     var spanElement = document.createElement('span');
-    spanElement.setAttribute('class','icon');
-    spanElement.append('x');
-    spanElement.setAttribute('onclick','removeMe(this)');
+    spanElement.setAttribute('class', 'del');
+    spanElement.append('X');
+    spanElement.setAttribute('onclick', 'removeMe(this)');
     list.append(spanElement);
 
-    var ul = document.getElementById('ul-list');   
+    var ul = document.getElementById('ul-list');
     ul.append(list);
 }
 
-function removeMe(e){
+function removeMe(e) {
     e.parentNode.remove();
 }
+
